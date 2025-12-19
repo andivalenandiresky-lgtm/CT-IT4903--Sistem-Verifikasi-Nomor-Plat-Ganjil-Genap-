9714faad7a65132c7fcd84417281fd0fa913892c
Fatmah_103032500034_IT-49-03 <fatmahshehzad07@gmail.com>
2025-12-19 15:37:56 +0700

    Update .go

--- .go
+++ .go
@@ -75,3 +75,112 @@ func main() {
 			if validBulan == false {
 				fmt.Println("Bulan tidak valid")
 			} else {
+				fmt.Print("Masukkan tahun: ")
+				fmt.Scanln(&tahun)
+
+				validTahun := true
+				i = 0
+
+				for i < len(tahun) {
+					if tahun[i] < '0' || tahun[i] > '9' {
+						validTahun = false
+					}
+					i = i + 1
+				}
+
+				if validTahun == false {
+					fmt.Println("Tahun tidak valid")
+				} else {
+
+					angkaTahun := 0
+					i = 0
+					for i < len(tahun) {
+						angkaTahun = angkaTahun*10 + int(tahun[i]-'0')
+						i = i + 1
+					}
+					validTanggal = true
+
+					h := angkaHari
+					m := angkaBulan
+					y := angkaTahun
+
+					if m < 3 {
+						m = m + 12
+						y = y - 1
+					}
+
+					k := y % 100
+					j := y / 100
+
+					hasil := (h + (13*(m+1))/5 + k + k/4 + j/4 + 5*j) % 7
+
+					namaHari := ""
+
+					if hasil == 0 {
+						namaHari = "Sabtu"
+					}
+					if hasil == 1 {
+						namaHari = "Minggu"
+					}
+					if hasil == 2 {
+						namaHari = "Senin"
+					}
+					if hasil == 3 {
+						namaHari = "Selasa"
+					}
+					if hasil == 4 {
+						namaHari = "Rabu"
+					}
+					if hasil == 5 {
+						namaHari = "Kamis"
+					}
+					if hasil == 6 {
+						namaHari = "Jumat"
+					}
+
+					fmt.Println("Hari:", namaHari)
+					if namaHari == "Sabtu" || namaHari == "Minggu" {
+						fmt.Println("Tidak berlaku ganjil genap (˶ > ₃ < ˶)")
+					} else {
+						var jenisKendaraan string
+						fmt.Print("Masukkan jenis kendaraan (listrik / nonlistrik): ")
+						fmt.Scanln(&jenisKendaraan)
+
+						if jenisKendaraan == "listrik" {
+							fmt.Println("Kendaraan listrik bebas melintas (˶˃𐃷˂˶)")
+						} else {
+							var plat string
+							fmt.Print("Masukkan dua digit terakhir plat: ")
+							fmt.Scanln(&plat)
+
+							validPlat := true
+							i = 0
+							for i < len(plat) {
+								if plat[i] < '0' || plat[i] > '9' {
+									validPlat = false
+								}
+								i = i + 1
+							}
+
+							if validPlat {
+								digitPlat := int(plat[len(plat)-1] - '0')
+								platGanjil := digitPlat%2 == 1
+								tanggalGanjil := angkaHari%2 == 1
+
+								if tanggalGanjil == platGanjil {
+									fmt.Println("KENDARAAN BOLEH MELINTAS (>⩊<)")
+								} else {
+									fmt.Println("KENDARAAN DILARANG MELINTAS (¬_¬) !!!")
+								}
+							} else {
+								fmt.Println("Plat tidak valid")
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
